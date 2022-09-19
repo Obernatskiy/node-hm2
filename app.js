89539acef489cd5591247cc4bc039130172cc243
Obernatskiy <sasha236541@gmail.com>
2022-09-19 12:53:44 +0300

    socket lesson

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
 const fileUpload = require('express-fileupload');
 require('dotenv').config();
 const mongoose = require('mongoose');
@@ -10,6 +12,36 @@ const {MONGO_URL} = require("./configs/config");
 const runCronJobs = require('./cron');
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {cors: 'http://localhost:63342'});
+
+io.on('connection', (socket) => {
+    console.log('=========================================================');
+    console.log(socket.id);
+    console.log(socket.handshake.auth);
+    console.log('=========================================================');
+
+    socket.on('message:create', (data) => {
+        console.log('data _________');
+        console.log(data);
+        console.log('data _________');
+    });
+
+
+    // EMIT EVENT TO SENDER
+    // socket.emit('user:create', { name: 'Socket', hard: 10 });
+
+    // EMIT EVENT ALL USERS INCLUDE SENDER
+    // io.emit('user:create', { name: 'Socket', hard: 10 });
+
+    // EMIT EVENT ALL USERS EXCLUDE SENDER
+    socket.broadcast.emit('user:create', {name: 'Socket', hard: 10});
+
+    socket.on('room:join', (data) => userJoinRoom(io, socket, data));
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -25,7 +57,7 @@ app.use('*', (req, res, next) => {
 
 app.use(mainErrorHandler)
 
-app.listen(5000, () => {
+server.listen(5000, () => {
     console.log('App listen 5000');
     mongoose.connect(MONGO_URL);
 
